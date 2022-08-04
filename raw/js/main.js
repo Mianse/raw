24f9386767fffdaf5c2a49bc625dfc940b285e55
Mianse <96182161+Mianse@users.noreply.github.com>
2022-08-05 05:22:32 +0800

    Add files via upload

---
+++ raw/js/main.js
@@ -0,0 +1,23 @@
+const companies= [
+    {name:"revere tech", category:"finance",start: 1997, end:2003},
+    {name:"cocacola", category:"technology",start: 1987, end:2007},
+    {name:"safaricom", category:"business",start: 1997, end:2003},
+    {name:"fanta", category:"retail",start: 1987, end:2002},
+    {name:"pepsi", category:"finance",start: 1997, end:2006},
+    {name:"emirates", category:"agriculture",start: 1987, end:2003},
+    {name:"brookside", category:"technology",start: 1997, end:2009},
+    {name:"chevrolet", category:"auto",start: 1977, end:2003},
+    {name:"mercedes", category:"finance",start: 1987, end:2008}
+];
+
+const ages= [18,34,23,43,23,45,42,34,35,65,16,18,19,27,26,29,30,];
+
+
+for(const i = 0;i < companies.length; i++){
+console.log(companies[i]);
+}
+//forEach
+//filter
+//map
+//sort
+//reduce
